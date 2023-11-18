82f4248a09031fcf7997b8d1cb958a31680fe20b
Taylor R. S. Allen <taylorrsallen@gmail.com>
2023-11-18 18:47:59 -0500

    DataAssets basic functionality complete

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -20,7 +20,7 @@ impl<T: Default + Clone + Serialize + for<'a> Deserialize<'a> + std::fmt::Debug 
     fn build(&self, app: &mut App) {
         let mut data_assets = DataAssets::<T>::new(&self.type_name);
         data_assets.init_directory();
-        data_assets.load();
+        data_assets.load_all();
         
         app.register_type::<DataAssets<T>>()
             .insert_resource(data_assets);
@@ -28,19 +28,12 @@ impl<T: Default + Clone + Serialize + for<'a> Deserialize<'a> + std::fmt::Debug 
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-#[derive(Default, Clone, Serialize, Deserialize, Debug, Reflect)]
-#[reflect(Default)]
-pub struct DataAsset<T: Default + Clone + Serialize + std::fmt::Debug + TypePath + FromReflect + Sync + Send + 'static> {
-    pub name: String,
-    pub data: T,
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
 #[derive(Resource, Default, Debug, Reflect)]
 #[reflect(Resource, Default)]
 pub struct DataAssets<T: Default + Clone + Serialize + for<'a> Deserialize<'a> + std::fmt::Debug + TypePath + FromReflect + Sync + Send + 'static> {
     pub type_name: String,
-    pub data: Vec<DataAsset<T>>,
+    data: Vec<T>,
+    asset_id_map: HashMap::<String, u16>,
 }
 
 impl<T: Default + Clone + Serialize + for<'a> Deserialize<'a> + std::fmt::Debug + TypePath + FromReflect + Sync + Send + 'static> DataAssets<T> {
@@ -50,6 +43,7 @@ impl<T: Default + Clone + Serialize + for<'a> Deserialize<'a> + std::fmt::Debug 
 
     pub fn full_path(&self) -> String { DATA_ASSET_DIR.to_string() + "/" + &self.type_name }
     pub fn asset_path(&self) -> String { "data/".to_string() + &self.type_name }
+    pub fn data(&self) -> &[T] { &self.data }
 
     pub fn init_directory(&self) {
         Serial::create_directory_path(self.full_path());
@@ -57,32 +51,39 @@ impl<T: Default + Clone + Serialize + for<'a> Deserialize<'a> + std::fmt::Debug 
     }
 
     fn create_example_assets(&self) {
-        let example_data = DataAsset::<T> { name: "example".into(), ..default() };
-        let multi_example_data = vec![example_data.clone(); 2];
-        Serial::save_type_to_ron(&example_data, self.full_path(), "example", 2);
-        Serial::save_type_to_ron(&multi_example_data, self.full_path(), "multi_example", 3);
+        Serial::save_type_to_ron(&T::default(), self.full_path(), "example", 1);
+    }
+
+    /// Saves based on asset names in `asset_id_map`. Will overwrite existing files.
+    pub fn save_all(&self) {
+        let full_path = self.full_path();
+
+        for (name, id) in self.asset_id_map.iter() {
+            let asset = self.data[*id as usize].clone();
+            Serial::save_type_to_ron(&asset, &full_path, name, 1);
+        }
     }
 
     /// Loads all data asset .ron files from `assets/data/{name}/*`
-    pub fn load(&mut self) {
+    pub fn load_all(&mut self) {
         let full_path = self.full_path();
         let files = Serial::file_paths_from_directory_recursive(&full_path, "");
-        let mut asset_map = HashMap::default();
+        let mut data_asset_map = HashMap::default();
 
         for file in files.iter() {
-            let Some(file) = file.strip_prefix("/") else { continue };
-            let Some(contents) = Serial::load_string_from_ron(&full_path, file) else { continue };
-            if let Ok(asset) = ron::from_str::<DataAsset<T>>(&contents) {
-                asset_map.insert(asset.name, asset.data);
-            } else if let Ok(assets) = ron::from_str::<Vec<DataAsset<T>>>(&contents) {
-                asset_map.extend(assets.iter().map(|asset| { (asset.name.clone(), asset.data.clone()) }));
-            }
+            let Some(file_name) = file.strip_prefix("/") else { continue };
+            let Some(asset) = Serial::load_type_from_ron::<&String, &str, T>(&full_path, file_name) else { continue };
+            data_asset_map.insert(file_name.to_string(), asset);
         }
 
-        asset_map.remove("example");
+        data_asset_map.remove("example");
 
-        for (name, asset) in asset_map.iter() {
-            println!("{name}: {:?}", asset);
+        let mut id = 0;
+        for (name, asset) in data_asset_map.iter() {
+            println!("[{id}] {name}: {:?}", asset);
+            self.asset_id_map.insert(name.clone(), id);
+            self.data.push(asset.clone());
+            id += 1;
         }
     }
 }
