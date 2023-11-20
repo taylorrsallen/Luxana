bd714f03592ca178511d67d3eab1116f6851180a
Taylor R. S. Allen <taylorrsallen@gmail.com>
2023-11-20 12:57:35 -0500

    Full refactor of Serial

--- src/util/serial.rs
+++ src/util/serial.rs
@@ -4,113 +4,91 @@ use serde::*;
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 pub struct Serial;
 impl Serial {
-    //==============================================================================================
-    pub fn save_type_to_ron<S0: AsRef<str>, S1: AsRef<str>, T: Serialize>(data: &T, path: S0, file_name: S1, depth_limit: usize) {
-        let path_string = path.as_ref().to_owned() + "/" + file_name.as_ref() + ".ron";
-        let path = Path::new(&path_string);
-        Self::save_from_ron_string(data, path, depth_limit);
+    /// Assumes that the last element in path is a file
+    pub fn create_file_path<P: AsRef<Path>>(path: P) -> bool {
+        let Some(prefix) = path.as_ref().parent() else { println!("Failed to get prefix path for {}", path.as_ref().display()); return false };
+        if std::fs::create_dir_all(prefix).is_ok() { true } else { println!("Failed to create path for {}", prefix.display()); false }
     }
 
-    pub fn load_type_from_ron<S0: AsRef<str>, S1: AsRef<str>, T: for<'a> Deserialize<'a>>(path: S0, file_name: S1) -> Option<T> {
-        let path_string = path.as_ref().to_owned() + "/" + file_name.as_ref() + ".ron";
-        let path = Path::new(&path_string);
-        Self::load_from_ron_string(path)
-    }
-
-    pub fn load_string_from_ron<S0: AsRef<str>, S1: AsRef<str>>(path: S0, file_name: S1) -> Option<String> {
-        let path_string = path.as_ref().to_owned() + "/" + file_name.as_ref() + ".ron";
-        let path = Path::new(&path_string);
-        Self::file_to_string(path)
-    }
-
-    pub fn load_type_or_default_from_ron<S0: AsRef<str>, S1: AsRef<str>, T: Default + Serialize + for<'a> Deserialize<'a>>(path: S0, file_name: S1, depth_limit: usize) -> T {
-        if let Some(data) = Self::load_type_from_ron(&path, &file_name) {
-            data
-        } else {
-            let data = T::default();
-            Self::save_type_to_ron(&data, path, file_name, depth_limit);
-            data
-        }
+    /// Assumes that the last element in path is a directory
+    pub fn create_directory_path<P: AsRef<Path>>(path: P) -> bool {
+        if std::fs::create_dir_all(path.as_ref()).is_ok() { true } else { println!("Failed to create path for {}", path.as_ref().display()); false }
     }
 
     //==============================================================================================
-    fn save_from_ron_string<P: AsRef<Path>, T: Serialize>(data: &T, path: P, depth_limit: usize) {
-        Self::create_file_path(&path);
-        if let Ok(contents) = Serial::to_ron_string_pretty(data, depth_limit) {
-            Serial::try_write_file(path, contents.as_bytes());
-        }
-    }
-
-    fn load_from_ron_string<P: AsRef<Path>, T: for<'a> Deserialize<'a>>(path: P) -> Option<T> {
-        if let Some(contents) = Serial::file_to_string(path) {
-            if let Ok(data) = ron::from_str(&contents) {
-                return Some(data);
-            }
-        }
-
-        None
-    }
-
-    //==============================================================================================
-    pub fn create_file_path<P: AsRef<Path>>(path: P) {
-        let prefix = path.as_ref().parent().unwrap();
-        if std::fs::create_dir_all(prefix).is_err() { println!("Failed to create path for {}", prefix.display()); }
-    }
-
-    pub fn create_directory_path<P: AsRef<Path>>(path: P) {
-        if std::fs::create_dir_all(path.as_ref()).is_err() { println!("Failed to create path for {}", path.as_ref().display()); }
-    }
-
-    //==============================================================================================
-    pub fn file_to_bytes<P: AsRef<Path>>(path: P) -> Option<Vec<u8>> {
-        if let Ok(file) = File::open(path) {
-            let mut buf_reader = BufReader::new(file);
-            let mut bytes: Vec<u8> = vec![];
-            
-            if let Ok(_) = buf_reader.read_to_end(&mut bytes) {
-                if bytes.len() != 0 {
-                    return Some(bytes);
-                }
-            }
-        }
-    
-        None
-    }
-    
-    pub fn file_to_string<P: AsRef<Path>>(path: P) -> Option<String> {
-        if let Ok(file) = File::open(path) {
-            let mut buf_reader = BufReader::new(file);
-            let mut contents = String::new();
-            
-            if let Ok(_) = buf_reader.read_to_string(&mut contents) {
-                if contents.as_str().len() != 0 {
-                    return Some(contents);
-                }
-            }
-        }
-    
-        None
-    }
-    
-    //==============================================================================================
-    fn try_write_file<P: AsRef<Path>>(path: P, bytes: &[u8]) -> bool {
-        if let Ok(mut file) = File::create(path) {
-            if let Ok(_) = file.write_all(bytes) {
-                return true;
-            }
-        }
-    
-        false
-    }
-    
-    fn to_ron_string_pretty<T: Serialize>(data: &T, depth_limit: usize) -> ron::Result<String> {
+    fn type_to_ron_string_with_depth_limit<T: Serialize>(data: &T, depth_limit: usize) -> ron::Result<String> {
         ron::ser::to_string_pretty(data, ron::ser::PrettyConfig::new().depth_limit(depth_limit))
     }
 
-    //==============================================================================================
-    // pub fn file_paths_from_directory_recursive_exclusive(directory: String, extension: String) -> Vec<String> {
+    /// Returns false if the file fails to create or to write
+    fn try_write_file<P: AsRef<Path>>(path: P, bytes: &[u8]) -> bool {
+        let Ok(mut file) = File::create(path) else { return false };
+        if file.write_all(bytes).is_ok() { true } else { false }
+    }
 
-    // }
+    /// Returns false if the file fails to create or to write
+    fn try_write_file_and_path<P: AsRef<Path>>(path: P, bytes: &[u8]) -> bool {
+        Self::create_file_path(&path);
+        Self::try_write_file(path, bytes)
+    }
+
+    //==============================================================================================
+    pub fn try_get_bytes_from_path<P: AsRef<Path>>(path: P) -> Option<Vec<u8>> {
+        let mut buf_reader = if let Ok(file) = File::open(path) { BufReader::new(file) } else { return None };
+        let mut bytes: Vec<u8> = vec![];
+        if buf_reader.read_to_end(&mut bytes).is_err() { return None; }
+        if bytes.len() != 0 { Some(bytes) } else { None }
+    }
+    
+    pub fn try_get_string_from_path<P: AsRef<Path>>(path: P) -> Option<String> {
+        let mut buf_reader = if let Ok(file) = File::open(path) { BufReader::new(file) } else { return None };
+        let mut contents = String::new();
+        if buf_reader.read_to_string(&mut contents).is_err() { return None; }
+        if contents.as_str().len() != 0 { Some(contents) } else { None }
+    }
+
+    //==============================================================================================
+    /// - `directory` should not end with `/`
+    /// - `file_name` can contain directories, but should not start with `/`, and the end should be only the name of the desired file
+    /// - `extension` should not include a `.`
+    pub fn path_string<S0: AsRef<str>, S1: AsRef<str>, S2: AsRef<str>>(directory: S0, file_name: S1, extension: S2) -> String {
+        directory.as_ref().to_owned() + "/" + file_name.as_ref() + "." + extension.as_ref()
+    }
+
+    //==============================================================================================
+    /// - `directory` should not end with `/`
+    /// - `file_name` can contain directories, but should not start with `/`, and the end should be only the name of the desired file
+    /// - `depth_limit` max indentations
+    pub fn save_type_to_ron_file<S0: AsRef<str>, S1: AsRef<str>, T: Serialize>(data: &T, directory: S0, file_name: S1, depth_limit: usize) {
+        let Ok(contents) = Self::type_to_ron_string_with_depth_limit(data, depth_limit) else { return };
+        Self::try_write_file_and_path(&Self::path_string(directory, file_name, "ron"), contents.as_bytes());
+    }
+
+    /// - `directory` should not end with `/`
+    /// - `file_name` can contain directories, but should not start with `/`, and the end should be only the name of the desired file
+    pub fn load_type_from_ron_file<S0: AsRef<str>, S1: AsRef<str>, T: for<'a> Deserialize<'a>>(directory: S0, file_name: S1) -> Option<T> {
+        let Some(contents) = Self::load_string_from_ron_file(directory, file_name) else { return None };
+        if let Ok(data) = ron::from_str(&contents) { Some(data) } else { None }
+    }
+
+    /// - `directory` should not end with `/`
+    /// - `file_name` can contain directories, but should not start with `/`, and the end should be only the name of the desired file
+    pub fn load_string_from_ron_file<S0: AsRef<str>, S1: AsRef<str>>(directory: S0, file_name: S1) -> Option<String> {
+        Self::try_get_string_from_path(&Self::path_string(directory, file_name, "ron"))
+    }
+
+    /// - `directory` should not end with `/`
+    /// - `file_name` can contain directories, but should not start with `/`, and the end should be only the name of the desired file
+    pub fn remove_ron_file<S0: AsRef<str>, S1: AsRef<str>>(directory: S0, file_name: S1) {
+        std::fs::remove_file(&Self::path_string(directory, file_name, "ron"));
+    }
+
+    //==============================================================================================
+    pub fn try_get_entry_name(entry: &DirEntry) -> Option<String> {
+        let path = entry.path();
+        let Some(file_stem) = path.file_stem() else { return None };
+        if let Some(stem_str) = file_stem.to_str() { Some(stem_str.to_string()) } else { None }
+    }
 
     /// `base_directory` will be added to the beginning of `search_directory`, but excluded from the paths returned.
     /// 
@@ -121,7 +99,7 @@ impl Serial {
         if let Ok(entries) = read_dir(&(base_directory.as_ref().to_owned() + "/" + search_directory.as_ref())) {
             for entry in entries {
                 let Ok(entry) = entry else { continue };
-                let Some(entry_name) = Serial::try_get_entry_name(&entry) else { continue };
+                let Some(entry_name) = Self::try_get_entry_name(&entry) else { continue };
                 let path = search_directory.as_ref().to_owned() + "/" + &entry_name;
                 if entry.path().is_dir() { directories.push(path); } else { files.push(path); }
             }
@@ -147,13 +125,5 @@ impl Serial {
         }
 
         files
-    }
-
-    pub fn try_get_entry_name(entry: &DirEntry) -> Option<String> {
-        if let Some(file_stem) = entry.path().file_stem() {
-            if let Some(stem_str) = file_stem.to_str() { Some(stem_str.to_string()) } else { None }
-        } else {
-            None
-        }
     }
 }
